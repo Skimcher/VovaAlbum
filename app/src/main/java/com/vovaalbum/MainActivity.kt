dccb8eaaf6ba37a617701d2ee819ae6ba7b0277f
Skimcher <zelenyilyg@gmail.com>
2026-06-04 13:55:55 +0300

    Create MainActivity.kt

---
+++ app/src/main/java/com/vovaalbum/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.vovaalbum
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.btnVova).setOnClickListener {
+            startActivity(Intent(this, Photo1Activity::class.java))
+        }
+    }
+}
